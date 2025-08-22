01162f113f6faab1f68f7a972b28f3a9186bee93
Yami4286 <abdulhanan8974@gmail.com>
2025-08-22 19:42:05 +0500

    Finished this part too, this lesson took me some days but soon we will be out of it

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(arr) {
+ let str = [...arr];
+ let i = str.length-1 ;
+ let reverse = "";
+ for(let s = i; s >= 0 ; s--){
+    reverse += str[s];
+     }
+     return reverse;
 };
-
+ 
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
