034b54818d80526f0f8d7bdd1c701ce20dd8b9d8
Yami4286 <abdulhanan8974@gmail.com>
2025-08-22 17:16:35 +0500

    Finally cleared it , when i opened it i had no clue wht to do but i finished it anyway

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (arr, times){
+    let yr = "";
+    if(times < 0){
+        return 'ERROR';
+    }
+ for(let i = 1; i <= times ; i++){
+    yr += arr;
+ }
+ return yr;
+}
 
-};
-
+ repeatString('hey',3);
+repeatString('hello',10);
+repeatString('hi',1);
+repeatString('bye',0);
+repeatString('goodbye',-1);
+repeatString('hey', 25);
+repeatString('', 10);
 // Do not edit below this line
 module.exports = repeatString;
