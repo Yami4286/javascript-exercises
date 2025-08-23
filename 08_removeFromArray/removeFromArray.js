1150131362c193cbb4ebae50393f1ed11ca77f89
Yami4286 <abdulhanan8974@gmail.com>
2025-08-23 18:32:57 +0500

    This is the next version i created it is small and pass all test with breeze

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,40 +1,23 @@
-const removeFromArray = function(arr, a,b) {
-    if(!arr.includes(a)){ a = null;}
-    if(!arr.includes(b)){b = null};
-    if(!arr.includes(a) && !arr.includes(b)){ return arr;}
-if(a == null || b == null){
-    if(a == null){ a = b;}
-    let as = arr.slice();
-    let Boo = true;
-while(Boo){
-    let bs = as.findIndex(fid => fid === a);
-as.splice(bs, 1);
-if(!as.includes(a)){
-    Boo = false;
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+const removeFromArray = function(arr, ...num) {
+    let ar = [];
+    for(let i = 0; i < arr.length ; i++){
+    if(!num.find(val => val === arr[i])){ar.push(arr[i]);}
 }
-}
-return as;
-}else{ 
-      let as = arr.slice();
-      let Boo =true;
-  while(Boo){
-          let bs = as.findIndex(fid => fid === a );
-as.splice(bs,1);
-let bc = as.findIndex(fid => fid === b );
-as.splice(bc,1);
-if(!as.includes(a) && !as.includes(b)){
- Boo=false;
-}
-  }
-return as;
-}
+return ar;
 };
+  
+
 
 removeFromArray([1, 2, 3, 4], 3);
 removeFromArray([1, 2, 3, 4], 3, 2);
 removeFromArray([1,2,2,3],2);
 removeFromArray([1, 2, 3, 4], 7, "tacos");
 removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 
 // Do not edit below this line
