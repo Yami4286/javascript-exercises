c92dd4c585206eaf5d30539bdfd3e179a261530a
Yami4286 <abdulhanan8974@gmail.com>
2025-08-23 23:23:26 +0500

    Finally finished this lesson it took too much time it took a week but finally finished

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhen) { 
+  let temop = (farhen - 32 )*(5/9);
+  return parseFloat(temop.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celcius) {
+  let temp = (Celcius *(9/5)) +32 ;
+  return parseFloat(temp.toFixed(1));
 };
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
