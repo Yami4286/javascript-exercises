e1b76057ace37b7b5c83638ba77aadbd5e52e24a
Yami4286 <abdulhanan8974@gmail.com>
2025-08-31 02:28:04 +0500

    Finally 14 done too, now last one is left

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+  parseInt(fib);
+  if(fib < 0){
+    return "OOPS";
+  }
+  if(fib === 1)
+{
+    return 0;
+}    
 
+let a = 0;
+    let b = 1;
+ let c;
+    for(let i = 0; i < fib; i++ ){
+        c = a + b;
+        b = a;
+        a = c;
+        
+    }
+    return a;
 };
-
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(0);
+fibonacci(-25);
+fibonacci("0");
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 // Do not edit below this line
 module.exports = fibonacci;
