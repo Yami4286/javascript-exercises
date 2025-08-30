5b3751b571429b4ef37f95a5a2fd328c2091bacd
Yami4286 <abdulhanan8974@gmail.com>
2025-08-30 17:28:28 +0500

    Yeah cleared this one too, just three more

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (srr) {
+   let fr = srr.toLowerCase().replace(/[^a-z0-9]/g,"");
+  const arr = fr.split("").reverse().join('');
+   if(arr === fr){
+       return true;
+   }
+   return false;
 };
+palindromes('racecar');
+palindromes('racecar!');
+palindromes('A car, a man, a maraca.');
+palindromes('Racecar!');
+palindromes('Animal loots foliated detail of stool lamina.');
+palindromes('ZZZZ car, a man, a maracaz.');
+palindromes('rac3e3car');
+palindromes('r3ace3car');
 
 // Do not edit below this line
 module.exports = palindromes;
