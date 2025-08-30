cae4aaa052dad4b6c8ba0a92e23c61bca9fdfdd5
Yami4286 <abdulhanan8974@gmail.com>
2025-08-31 03:54:41 +0500

    I was unable to complete one test but i mostly got it. had to look at solution but were finally done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+let way = array.reduce((name,value) => {
+ const oldestAge = getage(name.yearOfDeath,name.yearOfBirth);
+ const currentAge = getage(value.yearOfDeath,value.yearOfBirth);
+ return oldestAge > currentAge ? name : value;
+}, array[0]);
+return way;
+        
 };
+
+function getage(death,age){
+       if(!death){
+        death = new Date().getFullYear();;
+       }
+       return death - age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
